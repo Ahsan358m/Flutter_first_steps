9ccf89d2cb39e55ec81121fb3e570ee11426fd78
Ahsan358m <ahsan358m@gmail.com>
2023-04-14 02:55:41 +0500

    Day 10 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,50 @@
+class CatalogModel {
+  static final items = [
+    Item(
+      1,
+      "iPhone 12 Pro",
+      "Apple iPhone 12th generation",
+      300000,
+      "#33505a",
+      "https://cdn.shopify.com/s/files/1/0269/6580/3031/products/iphone-12-pro-max-gold-hero_dacc4e0b-839a-4fad-ad49-72bcde483af7_1024x1024.png?v=1658316898",
+    ),
+    Item(
+      2,
+      "Samsung S20FE",
+      "Samsung S20FE",
+      120000,
+      "#33505a",
+      "https://fundamental.in/wp-content/uploads/2021/07/233767_3_waayyl.png",
+    ),
+    Item(
+      3,
+      "iPhone SE 2022",
+      "Apple iPhone SE 2022",
+      30000,
+      "#33505a",
+      "https://www.applestore.pk/wp-content/uploads/2022/06/iPhone-SE-2022-apple-store-in-karachi.png",
+    ),
+    Item(
+      4,
+      "Samsung S23 Ultra",
+      "Samsung S23 Ultra",
+      350000,
+      "#33505a",
+      "https://api.sammobile.com/static/Galaxy%20S23%20Ultra_Product%20Image_Phantom%20Black_Devicepenbackr30_LI.png?1675244878",
+    ),
+    Item(
+      5,
+      "iPhone 14 Pro Max",
+      "Apple iPhone 14th generation",
+      450000,
+      "#33505a",
+      "https://w7.pngwing.com/pngs/60/414/png-transparent-iphone-14-thumbnail.png",
+    )
+  ];
+}
+
 class Item {
-  final String id;
+  final num id;
   final String name;
   final String desc;
   final num price;
@@ -8,14 +53,3 @@ class Item {
 
   Item(this.id, this.name, this.desc, this.price, this.color, this.image);
 }
-
-final products = [
-  Item(
-    '1',
-    "iPhone 12 Pro",
-    "Apple iPhone 12th generation",
-    300000,
-    "#33505a",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc",
-  )
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,53 @@
-import 'package:flutter/material.dart';
-import '../widgets/drawer.dart';
+import 'dart:ffi';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../widgets/drawer.dart';
+import "package:riverpod_flutter_starter/models/catalog.dart";
+import 'dart:convert';
+import '../widgets/item_widget.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedJson = jsonDecode(catalogJson);
+    var productData = decodedJson['products'];
+    print(productData);
+  }
 
   @override
   Widget build(BuildContext context) {
     final double version = 3.0;
     final int days = 456;
     final String name = 'Flutter $version';
+    final dummyList = List.generate(5, (index) => CatalogModel.items[index]);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Catalog App'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Hello $days Days of $name'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
